6ead18df29fb1603e9fff6a7b825a935139eacac
Mohamed Harmoush <mohamed.harmoush@check24.de>
2026-04-27 14:48:05 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="max-w-lg mx-auto bg-gray-100">
+      <h2 className="text-center m-4">Hello Todo App!</h2>
+    </div>
+  );
+};
+
+export default App;
