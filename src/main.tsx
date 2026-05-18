0b4b9634d5a9d36e905c882ab0894b14252bc2ae
Mohamed Harmoush <mohamed.harmoush@check24.de>
2026-05-18 16:43:21 +0200

    Add tanstack query packages

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,22 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import './index.css';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
 
 import App from './App.tsx';
 
+const queryClient = new QueryClient();
+
 createRoot(document.querySelector('#root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+    <ReactQueryDevtools />
+  </QueryClientProvider>,
 );
